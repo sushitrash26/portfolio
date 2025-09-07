098293cad99ab116a8c0cdffbe27e47747da7033
sushitrash <astitvanewemail@gmail.com>
2025-09-07 23:06:14 +0530

    build fixes

--- src/components/magicui/meteors.tsx
+++ src/components/magicui/meteors.tsx
@@ -30,7 +30,7 @@ export const Meteors = ({
     const styles = [...new Array(number)].map(() => ({
       "--angle": -angle + "deg",
       top: "-5%",
-      left: `calc(0% + ${Math.floor(Math.random() * window.innerWidth)}px)`,
+      left: `calc(0% + ${Math.floor(Math.random() * (typeof window !== 'undefined' ? window.innerWidth : 1200))}px)`,
       animationDelay: Math.random() * (maxDelay - minDelay) + minDelay + "s",
       animationDuration:
         Math.floor(Math.random() * (maxDuration - minDuration) + minDuration) +
